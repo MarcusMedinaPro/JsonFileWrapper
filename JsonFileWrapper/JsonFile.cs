d614e534fd6c117aca833306e4b7f4b32699f53e
Marcus Medina <medina.jobb@outlook.com>
2022-01-13 19:37:22 +0100

    Add project files.

---
+++ JsonFileWrapper/JsonFile.cs
@@ -0,0 +1,88 @@
+﻿// -----------------------------------------------------------------------------------------------
+//  JsonFile.cs by Marcus Medina, Copyright (C) 2021, http://MarcusMedina.Pro.
+//  Published under Apache License 2.0 (Apache-2.0)
+//  https://tldrlegal.com/license/apache-license-2.0-%28apache-2.0%29
+// -----------------------------------------------------------------------------------------------
+
+namespace JsonFileWrapper;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+
+/// <summary>
+/// Defines the <see cref="JsonFile{T}" />.
+/// </summary>
+/// <typeparam name="T">.</typeparam>
+public class JsonFile<T>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JsonFile{T}"/> class.
+    /// </summary>
+    /// <param name="filename">The filename<see cref="string"/>.</param>
+    public JsonFile (string filename)
+    {
+        Filename = filename;
+        Format = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+        };
+    }
+
+    /// <summary>
+    /// Gets or sets the Data object.
+    /// </summary>
+    public T? Data { get; set; } = default;
+
+    /// <summary>
+    /// Gets or sets the Filename.
+    /// </summary>
+    public string Filename { get; set; } = "";
+
+    /// <summary>
+    /// Gets or Sets the serialisation format settings.
+    /// </summary>
+    public JsonSerializerSettings Format { get; set; }
+
+    /// <summary>
+    /// Loads the file.
+    /// </summary>
+    public T? Load ()
+    {
+        var data = string.Empty;
+        if (File.Exists(Filename + ".json"))
+        {
+            try
+            {
+                data = File.ReadAllText(Filename + ".json");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                data = "{}";
+            }
+        }
+
+        try
+        {
+            Data = JsonConvert.DeserializeObject<T>(data);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+
+        return Data;
+    }
+
+    /// <summary>
+    /// Saves the file.
+    /// </summary>
+    public void Save () => File.WriteAllText(
+            Filename + ".json",
+            JsonConvert.SerializeObject(Data, null, Format));
+}
---
+++ JsonFileWrapperTests/JsonFileTests.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using JsonFileWrapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonFileWrapperTests
+{
+    [TestClass()]
+    public class JsonFileTests
+    {
+        /// <summary>
+        /// The filename.
+        /// </summary>
+        private const string filename = "Hello.txt";
+
+        /// <summary>
+        /// Cleans up the tests.
+        /// </summary>
+        [TestCleanup]
+        public void CleanUp ()
+        {
+            if (File.Exists(filename))
+                File.Delete(filename);
+        }
+
+        /// <summary>
+        /// Initialize the tests
+        /// </summary>
+        [TestInitialize]
+        public void Init ()
+        {
+            if (File.Exists(filename))
+                File.Delete(filename);
+        }
+
+        /// <summary>
+        /// Jsons the file test.
+        /// </summary>
+        [TestMethod()]
+        public void JsonFileTest ()
+        {
+            var file = new JsonFile<string>("");
+            Assert.IsNotNull(file);
+        }
+
+        /// <summary>
+        /// Loads a test.
+        /// </summary>
+        [TestMethod()]
+        public void LoadTest ()
+        {
+            // Arrange
+            var expected = "Hello Crazy World";
+            File.WriteAllText(filename, expected);
+            var file = new JsonFile<string>(filename);
+
+            // Act
+            file.Load();
+
+            // Assert
+            Assert.AreEqual(expected, file.Data);
+        }
+
+        /// <summary>
+        /// Saves a test.
+        /// </summary>
+        [TestMethod()]
+        public void SaveTest ()
+        {
+            //Arrange
+            var file = new JsonFile<string>(filename);
+            file.Data = "Hello Crazy World";
+
+            // Act
+            file.Save();
+
+            // Assert
+            Assert.IsNotNull(file);
+        }
+    }
+}
