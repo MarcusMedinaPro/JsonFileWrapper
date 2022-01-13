cd298c770cf03a63595128565b61c615b8d155be
Marcus Medina <medina.jobb@outlook.com>
2022-01-13 20:17:35 +0100

    Metainformation and icons

--- JsonFileWrapper/JsonFile.cs
+++ JsonFileWrapper/JsonFile.cs
@@ -1,7 +1,7 @@
 ﻿// -----------------------------------------------------------------------------------------------
 //  JsonFile.cs by Marcus Medina, Copyright (C) 2021, http://MarcusMedina.Pro.
 //  Published under Apache License 2.0 (Apache-2.0)
-//  https://tldrlegal.com/license/apache-license-2.0-%28apache-2.0%29
+//  https://www.tldrlegal.com/l/apache2
 // -----------------------------------------------------------------------------------------------
 
 namespace JsonFileWrapper;
